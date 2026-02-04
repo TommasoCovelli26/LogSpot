642f3544c64b5af33711f706cb59a111ca42ff2a
Marco Capriati <m.capriati19@studenti.uniba.it>
2026-02-04 18:26:38 +0100

    eliminazione page.tsx inutile in logopedista

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // Questa è la riga magica che risolve il problema di sqlite
+  serverExternalPackages: ["better-sqlite3"],
 };
 
 export default nextConfig;
